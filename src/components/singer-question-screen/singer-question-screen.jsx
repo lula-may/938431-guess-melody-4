df8601af42d2d3e3ff5f9fd09dd884ac66e4ead2
Yulia Elagina <julia-elagina@yandex.ru>
2020-06-18 17:08:20 +0300

    Преобразует компонент экрана вопроса об исполнителе обратно в функциональный

--- src/components/singer-question-screen/singer-question-screen.jsx
+++ src/components/singer-question-screen/singer-question-screen.jsx
@@ -1,77 +1,70 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const";
 
-class SingerQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const SingerQuestionScreen = (props) => {
+  const {onAnswer, question} = props;
+  const {
+    song,
+    answers
+  } = question;
 
-  render() {
-    const {onAnswer, question} = this.props;
-    const {
-      song,
-      answers
-    } = question;
+  return (
+    <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+        </a>
 
-    return (
-      <section className="game game--artist">
-        <header className="game__header">
-          <a className="game__back" href="#">
-            <span className="visually-hidden">Сыграть ещё раз</span>
-            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
-          </a>
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
+        </svg>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
-          </svg>
+        <div className="game__mistakes">
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+        </div>
+      </header>
 
-          <div className="game__mistakes">
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-          </div>
-        </header>
-
-        <section className="game__screen">
-          <h2 className="game__title">Кто исполняет эту песню?</h2>
-          <div className="game__track">
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio src={song.src}></audio>
-              </div>
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <button className="track__button track__button--play" type="button"></button>
+            <div className="track__status">
+              <audio src={song.src}></audio>
             </div>
           </div>
+        </div>
 
-          <form className="game__artist">
+        <form className="game__artist">
 
-            {answers.map((answer, i) => {
-              return (
-                <div key={answer.id} className="artist">
-                  <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`}
-                    id={`artist-${i}`}
-                    onChange={(evt) => {
-                      evt.preventDefault();
-                      onAnswer(question, answer);
-                    }}
-                  />
-                  <label className="artist__name" htmlFor={`artist-${i}`}>
-                    <img className="artist__picture" src={answer.avatar} alt={answer.singer}/>
-                    {answer.singer}
-                  </label>
-                </div>
+          {answers.map((answer, i) => {
+            return (
+              <div key={answer.id} className="artist">
+                <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`}
+                  id={`artist-${i}`}
+                  onChange={(evt) => {
+                    evt.preventDefault();
+                    onAnswer(question, answer);
+                  }}
+                />
+                <label className="artist__name" htmlFor={`artist-${i}`}>
+                  <img className="artist__picture" src={answer.avatar} alt={answer.singer}/>
+                  {answer.singer}
+                </label>
+              </div>
 
-              );
-            })}
+            );
+          })}
 
-          </form>
-        </section>
+        </form>
       </section>
-    );
-
-  }
-}
+    </section>
+  );
+};
 
 SingerQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
