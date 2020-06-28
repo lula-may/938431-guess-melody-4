9598b2ab76d9b056fb45dd07e09a07a727e97b8a
Yulia Elagina <julia-elagina@yandex.ru>
2020-06-28 17:55:53 +0300

    Создает e2e-test для Хока и исправляет тест для аудио-плеера

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -7,29 +7,15 @@ configure({
   adapter: new Adapter()
 });
 
+window.HTMLMediaElement.prototype.play = () => {};
+window.HTMLMediaElement.prototype.pause = () => {};
+
 const src = `test.ogg`;
 
-it(`Should run callback on play button click`, () => {
-  const onPlayBluttonClick = jest.fn();
-  const audioPlayer = mount(
-      <AudioPlayer
-        isPlaying={true}
-        onPlayButtonClick={onPlayBluttonClick}
-        src={src}
-      />
-  );
-  const playButton = audioPlayer.find(`button`);
-  playButton.props().onClick();
-
-  expect(onPlayBluttonClick).toHaveBeenCalledTimes(1);
-});
-
-describe(`Should set state property "isPlaying" on playButton click handler calling`, () => {
-  window.HTMLMediaElement.prototype.play = () => {};
-  window.HTMLMediaElement.prototype.pause = () => {};
+describe(`AudioPlayer Component`, () => {
   const onPlayBluttonClick = jest.fn();
 
-  it(`once to false`, () => {
+  it(`Should run callback on play button click`, () => {
     const audioPlayer = mount(
         <AudioPlayer
           isPlaying={true}
@@ -37,12 +23,30 @@ describe(`Should set state property "isPlaying" on playButton click handler call
           src={src}
         />
     );
-    audioPlayer.instance()._handleButtonClick();
+    const playButton = audioPlayer.find(`button`);
+    playButton.props().onClick();
+
+    expect(onPlayBluttonClick).toHaveBeenCalledTimes(1);
+  });
+
+
+  it(`Should set state property "isPlaying" to "false" on play button click`, () => {
+    const audioPlayer = mount(
+        <AudioPlayer
+          isPlaying={true}
+          onPlayButtonClick={onPlayBluttonClick}
+          src={src}
+        />
+    );
+    const playButton = audioPlayer.find(`button`);
+
+    audioPlayer.setState({isLoading: false});
+    playButton.simulate(`click`);
 
     expect(audioPlayer.state().isPlaying).toBe(false);
   });
 
-  it(`twice to true`, () => {
+  it(`Should set state property "isPlaying" to "true" on play button click twice`, () => {
     const audioPlayer = mount(
         <AudioPlayer
           isPlaying={true}
@@ -50,8 +54,10 @@ describe(`Should set state property "isPlaying" on playButton click handler call
           src={src}
         />
     );
-    audioPlayer.instance()._handleButtonClick();
-    audioPlayer.instance()._handleButtonClick();
+    const playButton = audioPlayer.find(`button`);
+    audioPlayer.setState({isLoading: false});
+    playButton.simulate(`click`);
+    playButton.simulate(`click`);
 
     expect(audioPlayer.state().isPlaying).toBe(true);
   });
---
+++ src/hocs/with-audio-player/with-audio-player.e2e.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import AudioPlayer from "../../components/audio-player/audio-player.jsx";
+import withActivePlayer from "./with-audio-player.jsx";
+import PropTypes from "prop-types";
+
+configure({
+  adapter: new Adapter()
+});
+
+const src = `test.ogg`;
+const id = 2;
+
+window.HTMLMediaElement.prototype.play = () => {};
+window.HTMLMediaElement.prototype.pause = () => {};
+
+it(`should set state property "activePlayerId" to id on playButton click and toggle playButton from "play" to "pause"`, () =>{
+  const MockComponent = ({renderPlayer}) => {
+    return (
+      <div>
+        {renderPlayer(src, id)}
+      </div>
+    );
+  };
+
+  const WithAudioPlayer = withActivePlayer(MockComponent);
+
+  const mockComponentWrapped = mount(
+      <WithAudioPlayer/>
+  );
+
+  const player = mockComponentWrapped.find(AudioPlayer);
+  const playButton = mockComponentWrapped.find(`button`);
+  player.setState({isLoading: false});
+
+  playButton.simulate(`click`, {});
+  expect(mockComponentWrapped.state().activePlayerId).toBe(id);
+  expect(playButton.hasClass(`track__button--pause`));
+
+  playButton.simulate(`click`, {});
+  expect(playButton.hasClass(`track__button--play`));
+
+  MockComponent.propTypes = PropTypes.func;
+});
